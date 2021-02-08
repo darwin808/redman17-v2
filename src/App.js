60d592764b6b0aba558e0bc3eea104dbc0cb2a30
darwin808 <you@domain.com>
2021-02-08 10:16:34 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  increment,
+  decrement,
+  addpeople,
+  deletepeople,
+  updatepeople,
+} from "./Actions/Actions";
+function App() {
+  const counter = useSelector((e) => e.CounterReducer);
+  const dispatch = useDispatch();
+  const peoples = useSelector((e) => e.PeopleReducer.peoples);
+  const [name, setname] = useState("");
+  const [edit, setedit] = useState("");
+
+  const [modal, setmodal] = useState(false);
+  const [idholder, setidholder] = useState(0);
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    dispatch(addpeople({ name, id: Math.random() }));
+  };
+  const openmodal = (id) => {
+    setidholder(id);
+    setmodal(true);
+  };
+  const handledit = (e) => {
+    e.preventDefault();
+    dispatch(updatepeople({ edit, idholder }));
+  };
+  return (
+    <div className="App">
+      {counter}
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}>
+        add
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}>
+        minus
+      </button>
+      <form action="submit" onSubmit={handlesubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => {
+            setname(e.target.value);
+          }}
+        />
+      </form>
+      {modal && (
+        <form action="submit" onSubmit={handledit}>
+          <input
+            type="text"
+            value={edit}
+            onChange={(e) => {
+              setedit(e.target.value);
+            }}
+          />
+        </form>
+      )}
+      {peoples.map((e) => (
+        <div key={e.id}>
+          {" "}
+          {e.name}{" "}
+          <button
+            onClick={() => {
+              dispatch(deletepeople(e.id));
+            }}>
+            delete
+          </button>
+          <button
+            onClick={() => {
+              openmodal(e.id);
+            }}>
+            edit
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Reducers/AllReducers.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import PeopleReducer from "./PeopleReducer";
+import CounterReducer from "./CounterReducer";
+const AllReducers = combineReducers({
+  PeopleReducer,
+  CounterReducer,
+});
+export default AllReducers;
